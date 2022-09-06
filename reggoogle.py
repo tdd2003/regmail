d6efbb83a10563f28091289816718d95d4af7f31
Tran Duc Duy Developer <109543062+tdd2003@users.noreply.github.com>
2022-09-06 20:23:20 +0700

    Add files via upload

---
+++ reggoogle.py
@@ -0,0 +1,26 @@
+import os,time,sys
+try: 
+    from faker import Faker
+    import random,re
+    from random import randint
+except:
+    os.system("pip install faker")
+from faker import Faker
+import random,re
+from random import randint
+
+faker=Faker()
+kytu = ['','_','.']
+soluong = int(input("Nhap so luong: "))
+luu = input("ten file: ")
+domain = input("ten domain: ")
+n = 0 
+for fake in range(soluong):
+    n+= 1
+    num = randint(10,200)
+    first_name = faker.first_name().lower()
+    last_name = faker.last_name().lower()
+    getkytu = random.choice(kytu)
+    facemail = first_name + last_name + str(num) +"@" + domain
+    print(" " " " +facemail)
+    open(luu,"a").write(facemail+"\n")
